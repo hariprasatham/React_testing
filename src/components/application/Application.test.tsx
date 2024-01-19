5f425dc91fc3b7f8fdb3566e73f6dd74daf511cd
hariprasatham <hariprasatham.com>
2024-01-19 13:53:34 +0530

    learning query

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -4,9 +4,49 @@ import Application from "./Application";
 describe("Application", ()=>{
     test("renders correctly", ()=>{
         render(<Application/>);
-        const nameElement = screen.getByRole("textbox")
+
+        const pageHeading = screen.getByRole("heading", {
+            level: 1
+        })
+        expect(pageHeading).toBeInTheDocument()
+
+        const sectionHeadingElement = screen.getByRole("heading", {
+            level: 2
+        })
+        expect(sectionHeadingElement).toBeInTheDocument()
+
+        const paragraphElement = screen.getByText("All fields are mandatory")
+        expect(paragraphElement).toBeInTheDocument()
+
+
+        const nameElement = screen.getByRole("textbox", {
+            name: "Name"
+        })
         expect(nameElement).toBeInTheDocument()
 
-        
+        const nameElement2 = screen.getByLabelText("Name", {
+            selector: "input"
+        })
+        expect(nameElement2).toBeInTheDocument()
+
+        const nameElement3 = screen.getByPlaceholderText("Fullname")
+        expect(nameElement3).toBeInTheDocument()
+
+        const bioElement = screen.getByRole("textbox", {
+            name: "Bio"
+        })
+        expect(bioElement).toBeInTheDocument()
+
+        const jobLocationElement = screen.getByRole("combobox")
+        expect(jobLocationElement).toBeInTheDocument()
+
+        const termsElement = screen.getByRole("checkbox")
+        expect(termsElement).toBeInTheDocument()
+
+        const termsElement2 = screen.getByLabelText("I agree to the terms and conditions")
+        expect(termsElement2).toBeInTheDocument()
+
+        const submitButtonElement = screen.getByRole("button")
+        expect(submitButtonElement).toBeInTheDocument()
     })
 })
--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 const Application = () => {
   return (
     <>
+    <h1>Job application form</h1>
+    <h2>Section 1</h2>
+    <p>All fields are mandatory</p>
     <form>
       <div>
         <label htmlFor="name">Name</label>
@@ -14,7 +17,11 @@ const Application = () => {
         />
       </div>
       <div>
-        <label htmlFor="job-location">Job location</label>
+        <label htmlFor="bio">Bio</label>
+        <textarea name="bio" id="bio"/>
+      </div>
+      <div>
+        <label htmlFor="job-location">Name</label>
         <select id="job-location">
           <option value="">Select a country</option>
           <option value="US">United States</option>
