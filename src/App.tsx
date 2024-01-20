2752d6d3a43f7bff752a9b5fa3c238198498fed0
hariprasatham <hariprasatham.com>
2024-01-20 10:26:35 +0530

    events

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Skills from './components/skills/Skills';
 import Application from './components/application/Application';
+import Counter from './components/counter/Counter';
 
 function App() {
   const skills = ["HTML", "CSS", "Js"]
   return (
     <div className="App">
-
+<Counter/>
     </div>
   );
 }
